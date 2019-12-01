2fe2220368481747655131e7b138e19481fc7cb9
Cheungoo <57555001+Cheungoo@users.noreply.github.com>
2019-12-01 14:08:15 +0800

    Add files via upload

---
+++ byebye.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class byebye : MonoBehaviour
+{
+    public void over_back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
